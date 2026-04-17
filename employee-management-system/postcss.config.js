3e99e214badc67855584533352f2da3f92580ccc
Arth1803 <arthgahlod2003@gmail.com>
2026-04-17 22:34:07 +0530

    fix postcss config

--- employee-management-system/postcss.config.js
+++ employee-management-system/postcss.config.js
@@ -1,1 +1,8 @@
-export default {plugins: { tailwindcss: {}, autoprefixer: {}, } }
+// export default {plugins: { tailwindcss: {}, autoprefixer: {}, } }
+
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
